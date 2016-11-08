e85afd17936de5766d0df5b4be23cf9683bb6336
thiagobustamante <thiago@cruxframework.org>
2016-11-08 14:04:08 -0200

    Add bin and doc folders to gitignore

--- bin/lib/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var gateway_1 = require("./gateway");
-var command_line_1 = require("./command-line");
-var gateway = new gateway_1.Gateway(command_line_1.Parameters.gatewayConfigFile);
-gateway.start(function () {
-    gateway.startAdmin();
-});
-
-//# sourceMappingURL=app.js.map
--- bin/lib/config/serviceDiscovery.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Joi = require("joi");
-exports.ServiceDiscoveryConfigValidatorSchema = Joi.object().keys({});
-function validateServiceDiscoveryConfig(serviceDiscovery, callback) {
-    Joi.validate(serviceDiscovery, exports.ServiceDiscoveryConfigValidatorSchema, callback);
-}
-exports.validateServiceDiscoveryConfig = validateServiceDiscoveryConfig;
-
-//# sourceMappingURL=serviceDiscovery.js.map
