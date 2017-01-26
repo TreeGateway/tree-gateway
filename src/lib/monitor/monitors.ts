684dd7ead28244519772937407d53dc45aa75e8e
thiagobustamante <thiago@cruxframework.org>
2017-01-26 16:15:26 -0200

    fix active cluster nodes list

--- src/lib/monitor/monitors.ts
+++ src/lib/monitor/monitors.ts
@@ -9,6 +9,7 @@ import * as _ from "lodash";
 export class Monitors {
     static MONITORS_PREFIX: string = "monitors"
     static activeMonitors: Array<Monitor> = new Array<Monitor>();
+    private static interval: NodeJS.Timer;
 
     static startMonitors(gateway: Gateway){
         if (gateway.config.monitor) {
@@ -50,17 +51,34 @@ export class Monitors {
         return new Promise<Array<string>>((resolve, reject) => {
             gateway.redisClient.hgetall(Monitors.MONITORS_PREFIX)
                 .then((monitors) => {
-                    resolve(_.keys(monitors));
+                    let result: Array<string> = [];
+                    let now = Date.now();
+                    _.keys(monitors).forEach(machine => {
+                        if (now-monitors[machine] < 10001) {
+                            result.push(machine);
+                        }
+                        else {
+                            gateway.redisClient.hdel(`${Monitors.MONITORS_PREFIX}`, machine);
+                        }
+                    });
+                    resolve(result);
                 })
                 .catch(reject);
-        });
-        
+        });        
     }
 
     private static registerMonitor(gateway: Gateway) {
-        gateway.redisClient.hmset(`${Monitors.MONITORS_PREFIX}`, Monitor.getMachineId(), Date.now());
+        let period = 10000;
+        Monitors.interval = setInterval(()=>{
+            gateway.redisClient.hmset(`${Monitors.MONITORS_PREFIX}`, Monitor.getMachineId(), Date.now());
+        }, period);
     }
+
     private static unregisterMonitor(gateway: Gateway) {
+        if (Monitors.interval) {
+            clearInterval(Monitors.interval);
+            Monitors.interval = null;
+        }
         gateway.redisClient.hdel(`${Monitors.MONITORS_PREFIX}`, Monitor.getMachineId());
     }
     
