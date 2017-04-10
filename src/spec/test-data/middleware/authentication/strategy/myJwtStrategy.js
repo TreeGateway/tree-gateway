bba0585e6cc3b5dfdd39d92350378cac0e2cd765
thiagobustamante <thiago@cruxframework.org>
2017-04-10 17:33:16 -0300

    Fix test data

---
+++ src/spec/test-data/middleware/authentication/strategy/myJwtStrategy.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+
+module.exports = function (authConfig) {
+    var opts = {}
+    opts.jwtFromRequest = ExtractJwt.fromUrlQueryParameter('jwt');
+    opts.secretOrKey = authConfig.secret;
+    return new JwtStrategy(opts, function(jwt_payload, done) {
+        done(null,jwt_payload.sub);
+    });
+}; 
