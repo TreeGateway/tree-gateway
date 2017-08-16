4a3564d9d19d7cf32482a7c1034fc08b962d3c9f
thiagobustamante <thiago@cruxframework.org>
2017-08-16 11:00:10 -0300

    Fix #11

---
+++ src/proxy/routers/loadBalancer.ts
@@ -0,0 +1,185 @@
+'use strict';
+
+import * as Joi from 'joi';
+import * as _ from 'lodash';
+import * as chooser from 'weighted';
+import { ValidationError } from '../../error/errors';
+import { Container } from 'typescript-ioc';
+import { getMilisecondsInterval } from '../../utils/time-intervals';
+import { PluginsDataService } from '../../service/plugin-data';
+import { HealthCheck } from '../../utils/health-check';
+
+interface LoadBalancerConfig {
+    destinations: Array<Destination>;
+    database?: LoadBalancerDatabaseConfig;
+    /**
+     * The Load Balancer strategy used to choose one between the available service nodes. Defaults to random.
+     */
+    strategy?: string;
+}
+
+interface LoadBalancerDatabaseConfig {
+    key?: string;
+    checkInterval?: string | number;
+}
+
+interface Destination {
+    target: string;
+    weight?: number;
+    healthCheck?: string;
+    isDown?: boolean;
+}
+
+const loadBalancerConfigSchema = Joi.object().keys({
+    database: Joi.object().keys({
+        checkInterval: Joi.alternatives([Joi.string(), Joi.number().positive()]),
+        key: Joi.string()
+    }),
+    destinations: Joi.array().items(Joi.object().keys({
+        healthCheck: Joi.string(),
+        target: Joi.string().required(),
+        weight: Joi.number()
+    })).min(1).required(),
+    healthCheck: Joi.string(),
+    strategy: Joi.string().valid('random', 'round-robin', 'weight')
+});
+
+function validateLoadBalancerConfig(config: LoadBalancerConfig) {
+    const result = Joi.validate(config, loadBalancerConfigSchema);
+    if (result.error) {
+        throw new ValidationError(result.error);
+    } else {
+        return result.value;
+    }
+}
+
+abstract class Balancer {
+    protected verifiedInstances: Array<Destination>;
+    protected serviceInstances: Array<Destination>;
+    protected fixedServiceInstances: Array<Destination>;
+    protected healthChecker: HealthCheck;
+    protected previousDBData: Array<string>;
+
+    constructor(config: LoadBalancerConfig) {
+        this.fixedServiceInstances = config.destinations;
+        this.updateInstances(this.fixedServiceInstances);
+        this.observeDatabase(config);
+    }
+
+    protected observeDatabase(config: LoadBalancerConfig) {
+        if (config.database) {
+            const pluginsDataService: PluginsDataService = Container.get(PluginsDataService);
+            pluginsDataService.on('changed', (data: Array<string>) => {
+                if (!_.isEqual(this.previousDBData, data)) {
+                    this.previousDBData = data;
+                    if (data && data.length) {
+                        const dbConfig: Array<Destination> = data.map(item => JSON.parse(item));
+                        this.updateInstances(this.mergeInstances(dbConfig));
+                    } else {
+                        this.updateInstances(this.mergeInstances(null));
+                    }
+                }
+            });
+            pluginsDataService.watchConfigurationItems(config.database.key || 'loadBalancer:instances',
+                                        getMilisecondsInterval(config.database.checkInterval, 30000));
+        }
+    }
+
+    protected mergeInstances(data: Array<Destination>) {
+        if (data && data.length) {
+            return _.unionWith(this.fixedServiceInstances, data, _.isEqual);
+        }
+        return this.fixedServiceInstances;
+    }
+
+    protected updateInstances(data: Array<Destination>) {
+        this.serviceInstances = data;
+        this.checkInstances(data);
+    }
+
+    protected checkInstances(data: Array<Destination>) {
+        const monitoredInstances = data.filter(destination => destination.healthCheck);
+        if (monitoredInstances && monitoredInstances.length) {
+            if (this.healthChecker) {
+                this.healthChecker.stop();
+                this.healthChecker.removeAllListeners();
+            }
+            this.healthChecker = new HealthCheck({
+                servers: monitoredInstances.map(server => server.healthCheck)
+            });
+            this.healthChecker.on('change', (servers: any) => {
+                Object.keys(servers).forEach(server => {
+                    const index = _.findIndex(this.serviceInstances, (instance) => instance.healthCheck === server);
+                    if (index >= 0) {
+                        this.serviceInstances[index].isDown = servers[server].down;
+                    }
+                });
+            });
+            this.verifiedInstances = this.serviceInstances.filter(server => !server.isDown);
+        } else {
+            this.verifiedInstances = this.serviceInstances;
+        }
+    }
+
+    abstract balance(): string;
+}
+
+class RandomBalancer extends Balancer {
+    constructor(config: LoadBalancerConfig) {
+        super(config);
+    }
+
+    balance(): string {
+        return this.verifiedInstances[Math.floor(Math.random() * this.verifiedInstances.length)].target;
+    }
+}
+
+class RoundRobinBalancer extends Balancer {
+    constructor(config: LoadBalancerConfig) {
+        super(config);
+    }
+    balance(): string {
+        let next = (<any>this.verifiedInstances).next || 0;
+        const index = next % this.verifiedInstances.length;
+        next++;
+        (<any>this.verifiedInstances).next = next;
+        return this.verifiedInstances[index].target;
+    }
+}
+
+class WeightedBalancer extends Balancer {
+    private instances: Array<string>;
+    private weights: Array<number>;
+
+    constructor(config: LoadBalancerConfig) {
+        super(config);
+    }
+
+    protected updateInstances(data: Array<Destination>) {
+        super.updateInstances(data);
+        this.instances = this.verifiedInstances.map(item => item.target);
+        this.weights = this.verifiedInstances.map(item => item.weight);
+    }
+
+    balance(): string {
+        return chooser.select(this.instances, this.weights);
+    }
+}
+
+module.exports = function(config: LoadBalancerConfig) {
+    validateLoadBalancerConfig(config);
+    let balancer: Balancer;
+    switch(config.strategy) {
+        case 'round-robin':
+            balancer = new RoundRobinBalancer(config);
+            break;
+        case 'weight':
+            balancer = new WeightedBalancer(config);
+            break;
+        default:
+            balancer = new RandomBalancer(config);
+    }
+    return (req: any) => {
+        return balancer.balance();
+    };
+};
---
+++ src/utils/health-check.ts
@@ -0,0 +1,172 @@
+'use strict';
+
+import * as http from 'http';
+import * as https from 'https';
+import * as url from 'url';
+import * as _ from 'lodash';
+import { EventEmitter } from 'events';
+import { getMilisecondsInterval } from './time-intervals';
+import { Logger } from '../logger';
+import { Inject } from 'typescript-ioc';
+
+export interface HealthCheckOptions {
+    delay?: string | number;
+    failcount?: number;
+    servers: Array<string>;
+    timeout?: string | number;
+}
+
+export interface CheckServer {
+    down: boolean;
+    failcount: number;
+    lastStatus: string;
+}
+
+export class HealthCheck extends EventEmitter {
+    @Inject private logger: Logger;
+
+    private checks: any = {};
+    private previousChecks: any;
+    private options: HealthCheckOptions;
+    private interval: number;
+    private checking: boolean = false;
+
+    constructor(options: HealthCheckOptions) {
+        super();
+        this.options = _.defaults(options, {
+            delay: 30000,
+            failcount: 2,
+            timeout: 2000
+        });
+
+        if (options && options.servers && options.servers.length > 0) {
+            options.servers.forEach((s) => {
+                this.checks[s] = {
+                    down: false,
+                    failcount: 0,
+                    lastStatus: ''
+                };
+            });
+            this.start();
+        }
+    }
+
+    start() {
+        if (!this.interval) {
+            this.check();
+            this.interval = setInterval(() => {
+                this.check();
+            }, getMilisecondsInterval(this.options.delay, 30000));
+        }
+    }
+
+    stop() {
+        if (this.interval) {
+            clearInterval(this.interval);
+            this.interval = 0;
+        }
+    }
+
+    isDown(serverUrl: string) {
+        const hc = this.checks[serverUrl];
+        if (hc) {
+            return hc.down;
+        } else {
+            return new Error(`healthcheck: Unknow server: ${name}`);
+        }
+    }
+
+    status() {
+        return Object.assign({}, this.checks);
+    }
+
+    private check() {
+        if (!this.checking) {
+            this.checking = true;
+            this.previousChecks = this.status();
+            const promises = new Array<Promise<CheckServer>>();
+            _.forIn(this.checks, (config, urlServer) => {
+                promises.push(this.checkServer(urlServer, config));
+            });
+
+            Promise.all(promises)
+                .then((result) => {
+                    this.checking = false;
+                    if (!_.isEqual(this.previousChecks, this.checks)) {
+                        if (this.logger.isDebugEnabled()) {
+                            this.logger.debug(`Health state changed: ${this.logger.inspectObject(this.checks)}`);
+                        }
+                        this.emit('change', this.status());
+                    }
+                }).catch(err => {
+                    this.checking = false;
+                });
+        }
+    }
+
+    private checkServer(urlServer: string, config: CheckServer) {
+        return new Promise<CheckServer>((resolve, reject) => {
+            if (this.logger.isDebugEnabled()) {
+                this.logger.debug(`Checking server health: ${urlServer}`);
+            }
+
+            const urlCheck = url.parse(urlServer);
+            let ended = false;
+
+            const library: any = urlCheck.protocol === 'https' ? https : http;
+            const request: any = library.get({
+                agent: false,
+                host: urlCheck.hostname,
+                path: urlCheck.pathname,
+                port: urlCheck.port
+            }, (response: http.ClientResponse) => {
+                let result = new Buffer('');
+                if (response.statusCode === 200) {
+                    response.on('data', (chunk: Buffer) => {
+                        result = Buffer.concat([result, chunk]);
+                    });
+                    response.on('end', () => {
+                        if (ended) {
+                            return null;
+                        }
+                        config.lastStatus = 'OK';
+                        config.failcount = 0;
+                        config.down = false;
+                        resolve(config);
+                    });
+                } else {
+                    config.lastStatus = 'Non 200 HTTP status code';
+                    config.failcount += 1;
+                    if (config.failcount >= this.options.failcount) {
+                        config.down = true;
+                        resolve(config);
+                    }
+                }
+            });
+
+            request.on('socket', (socket: any) => {
+                socket.setTimeout(getMilisecondsInterval(this.options.timeout, 2000));
+                socket.on('timeout', () => {
+                    ended = true;
+                    request.abort();
+                    config.lastStatus = 'Healthcheck timed out';
+                    config.failcount += 1;
+                    if (config.failcount >= this.options.failcount) {
+                        config.down = true;
+                    }
+                    resolve(config);
+                });
+            });
+
+            request.on('error', (error: any) => {
+                ended = true;
+                config.lastStatus = error.message;
+                config.failcount += 1;
+                if (config.failcount >= this.options.failcount) {
+                    config.down = true;
+                }
+                resolve(config);
+            });
+        });
+    }
+}
