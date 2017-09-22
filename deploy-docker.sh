81611d5aeb22757801f0a141cb13f7d06cd31c3a
thiagobustamante <thiago@cruxframework.org>
2017-09-22 17:35:33 -0300

    Fix travis

---
+++ deploy-docker.sh
@@ -0,0 +1,6 @@
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+export REPO=treegateway/tree-gateway
+export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "master"; else echo $TRAVIS_BRANCH ; fi`
+docker build -f Dockerfile -t $REPO:$TAG .
+docker tag $REPO:$TAG $REPO:latest
+docker push $REPO
