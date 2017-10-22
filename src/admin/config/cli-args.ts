b0138548b033f3577144313590b3a51070164e25
thiagobustamante <thiago@cruxframework.org>
2017-10-22 02:13:01 -0200

    new version

--- src/admin/config/cli-args.ts
+++ src/admin/config/cli-args.ts
@@ -6,7 +6,7 @@ import { Configuration } from '../../configuration';
 const parser = new ArgumentParser({
     addHelp: true,
     description: 'Tree-Gateway Admin',
-    version: '1.1.6'
+    version: '1.1.7'
 });
 
 parser.addArgument(
