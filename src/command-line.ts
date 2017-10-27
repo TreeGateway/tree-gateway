bd75fd570804a44b3447c68160d0a26f885a241c
thiagobustamante <thiago@cruxframework.org>
2017-10-27 09:52:55 -0200

    new version

--- src/command-line.ts
+++ src/command-line.ts
@@ -6,7 +6,7 @@ import { Configuration } from './configuration';
 const parser = new ArgumentParser({
     addHelp: true,
     description: 'Tree-Gateway',
-    version: '1.1.11'
+    version: '1.1.12'
 });
 
 parser.addArgument(
