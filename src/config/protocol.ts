e810ac0e04dd035012662c97bf852188030c9c9c
Thiago Bustamante <trbustamante@gmail.com>
2018-06-30 16:52:59 -0300

    Support cipher configurations for HTTPS

--- src/config/protocol.ts
+++ src/config/protocol.ts
@@ -27,6 +27,19 @@ export interface HttpsConfig {
      * Path to the certificate file.
      */
     certificate: string;
+    /**
+     * Path to the certificateAuthority file.
+     */
+    certificateAuthority?: string;
+    /**
+     * A list of Ciphers to use.
+     */
+    ciphers?: Array<string>;
+    /**
+     * When choosing a cipher, use the server's preferences
+     * instead of the client preferences.
+     */
+    honorCipherOrder?: boolean;
 }
 
 const httpConfigSchema = Joi.object().keys({
@@ -35,6 +48,9 @@ const httpConfigSchema = Joi.object().keys({
 
 const httpsConfigSchema = Joi.object().keys({
     certificate: Joi.string().required(),
+    certificateAuthority: Joi.string(),
+    ciphers: Joi.array().items(Joi.string()),
+    honorCipherOrder: Joi.boolean(),
     listenPort: Joi.alternatives([Joi.number().positive(), Joi.string()]).required(),
     privateKey: Joi.string().required()
 
